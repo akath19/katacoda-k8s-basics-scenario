466b331632cafccb6e124c1da681f7d4664df0c4
Julián Camilo Cuevas A <akath19@gmail.com>
2022-04-29 10:38:55 -0500

    Simplified validation for scenario 3

--- yp-challenges/secure-configuration/validation.sh
+++ yp-challenges/secure-configuration/validation.sh
@@ -2,15 +2,5 @@
 curl http://localhost:3000
 kubectl get secret grafana-secrets
 kubectl get secret postgres-secrets
-grafana=$(kubectl get deploy grafana -o jsonpath='{.spec.template.spec.containers[0].envFrom[0].secretRef.name}')
-postgres=$(kubectl get deploy postgres -o jsonpath='{.spec.template.spec.containers[0].envFrom[0].secretRef.name}')
-
-if [ "$grafana" == "grafana-secrets" ]; then
-  if [ "$postgres" == "postgres-secrets" ]; then
-    exit 0
-  else
-    exit 1
-  fi
-else
-  exit 1
-fi
+grafana=$(kubectl get deploy grafana -o jsonpath='{.spec.template.spec.containers[0].envFrom[0].secretRef.name=="grafana-secrets"}')
+postgres=$(kubectl get deploy postgres -o jsonpath='{.spec.template.spec.containers[0].envFrom[0].secretRef.name=="postgres-ecret"}')
